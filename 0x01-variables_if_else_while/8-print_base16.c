827033d1a9e9f54c490b0ea95268408be51bbb23
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-04-08 17:27:22 -0400

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,20 +8,20 @@
  */
 int main(void)
 {
-	char d = '0';
-	while (d <= '9')
-	{
-		putchar(d);
-		d++;
-	}
-	char c = 'a';
-	while (c <= 'f')
-	{
-		putchar(c);
-		c++;
-	}
+char d = '0';
+while (d <= '9')
+{
+	putchar(d);
+	d++;
+}
+char c = 'a';
+while (c <= 'f')
+{
+	putchar(c);
+	c++;
+}
 
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
 
