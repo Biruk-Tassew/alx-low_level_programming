9109aebb5d17a7b045a0de05c24324bc66b22278
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-04-08 17:45:23 -0400

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,25 +21,21 @@ while (f < 10)
 		d = 0;
 		while (d < 10)
 		{
-			c = 0;
-			while (c < 10)
+			if (!(f == c && e == d))
 			{
-				if (!(f == c && e == d))
+				putchar('0' + f);
+				putchar('0' + e);
+				putchar(' ');
+				putchar('0' + d);
+				putchar('0' + c);
+				if (!(f + e == 18 && c + d == 17 && d == 9))
 				{
-					putchar('0' + f);
-					putchar('0' + e);
+					putchar(',');
 					putchar(' ');
-					putchar('0' + d);
-					putchar('0' + c);
-					if (!(f + e == 18 && c + d == 17 && d == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
-				c++;
 			}
-			d++;
+			
+		d++;
 		}
 		e++;
 	}
