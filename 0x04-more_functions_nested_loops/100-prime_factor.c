8b0d1a477852801f223a0f7ceaec913a49fc389c
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-04-14 13:26:44 -0400

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+long int num, factor;
+
+n = 612852475143;
+for (factor = 2; factor <= num; factor++)
+{
+if (num % factorp == 0)
+{
+num /= factor;
+factor--;
+}
+}
+printf("%ld\n", factor);
+return (0);
+}
