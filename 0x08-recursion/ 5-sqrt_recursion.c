8ac5c1539f3a3913f48d54ff23b31d4fbcccba90
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-04-27 11:29:54 -0400

    Update  5-sqrt_recursion.c

---
+++ 0x08-recursion/ 5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * checker - checks the input number from n to the base
+ * @n: number is squared and compared against base
+ * @base: base number to check
+ * Return: natural square root of number base
+ */
+int sqrt_check(int n, int base)
+{
+	if (n * n == base)
+		return (n);
+	if (n * n > base)
+		return (-1);
+	return (checker(n + 1, base));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ *
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (sqrt_check(1, n));
+}
