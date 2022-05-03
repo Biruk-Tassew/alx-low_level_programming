12aa42c6bc84f34d9254660c74d12473e03b6911
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-05-03 12:15:14 -0400

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,17 @@ char *create_array(unsigned int size, char c)
 {
 	char *array = NULL;
 	unsigned int i;
-	if (size == 0) return (NULL);
-	array = (char *)malloc(size * sizeof(char));
-	if (array != NULL)
+
+	if (size == 0)
+		return (NULL);
+	if (size != 0)
 	{
-		for (i = 0; i < size; i++)
-			array[i] = c;
+		array = (char *)malloc(size * sizeof(char));
+		if (array != NULL)
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+		}
 	}
 	return (array);
 }
