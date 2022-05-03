e150b69c7072af4dfda284842fde85b779d853ea
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-05-03 12:17:03 -0400

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -15,34 +15,27 @@ char *pointeur;
 int i;
 int count = 0;
 
-/* in case if the given string is NULL*/
 if (str == NULL)
 	return (NULL);
 
-/* finding the length of the given string*/
 while (*(str + count))
 
 	count++;
-/*allocation the needed dynamic memory area */
+
 pointeur = malloc(sizeof(char) * (count + 1));
 
 
-/* to verify if the pointer , pointed on the allocated memory*/
-/* area is not NULL */
 if (pointeur == NULL)
 	return (NULL);
 
-/* initialization of i to avoid segment fault error*/
 i = 0;
 
-/* filling the allocated area with the bytes from the given string */
 while (i <= count)
 {
 	*(pointeur + i) = *(str + i);
 		i++;
 }
 
-/* return a pointer to the string */
 return (pointeur);
 }
 
