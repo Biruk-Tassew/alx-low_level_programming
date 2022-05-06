8ed331d51bd78320ee7bbe1be3cdcbd73da18964
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-05-06 10:59:33 -0400

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -11,21 +11,21 @@
 int *array_range(int min, int max)
 {
 	int i, l;
-	int *mem;
+	int *loc;
 
 	if (min > max)
 		return (NULL);
-	l = max - min + 1;
+	len = max - min + 1;
 /* dynamic memory space allocation */
-	mem = malloc(sizeof(int) * l);
+	loc = malloc(sizeof(int) * len);
 /* memory allocation fail exit */
-	if (mem == NULL)
+	if (loc == NULL)
 		return (NULL);
 /*filling the array with numbers from min to max */
 	for (i = min; i <= max; i++)
 	{
-		mem[i - min] = i;
+		loc[i - min] = i;
 	}
 
-	return (mem);
+	return (loc);
 }
