df001455dce10dc06bbcd8e1c3c925c9d722389b
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-05-06 10:46:44 -0400

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -12,13 +12,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 	unsigned int i, size1 = 0, size2 = 0;
-/*checking if the 2 strings are empty */
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-/* calculating the length of both strings */
+	
 	while (*(s1 + size1) != '\0')
 		size1++;
 	while (*(s2 + size2) != '\0')
@@ -26,11 +25,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (size2 >= n)
 		size2 = n;
-/* allocating the needed memory for the 2 strings*/
 	s = malloc(size1 + n + 1);
 	if (s == NULL)
 		return (s);
-/* copying the first string to the allacoted memory */
 	for (i = 0; i < size1; i++)
 		s[i] = s1[i];
 
