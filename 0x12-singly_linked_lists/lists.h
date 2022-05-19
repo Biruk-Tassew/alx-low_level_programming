fd6a63adc1e2fbaacfe8211ff10ae4bc34d19819
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-05-19 08:59:35 -0400

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Coding project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 size_t list_len(const list_t *h);
 
 
