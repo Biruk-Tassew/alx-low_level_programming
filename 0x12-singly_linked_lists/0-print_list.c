5bfd5afc2a7d7c15f5a259257f9931c9749137a8
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-05-19 08:53:58 -0400

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include <lists.h>
+
+/**
+ * print_list - this function print the values of nodes in a linked list
+ * @h: pointer to struct
+ *
+ * Description: prints all the elements of a list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+        int count = 0;
+
+	      while (h)
+	      {
+		            printf("%d\n", (*h).n);
+		            h = (*h).next;
+		            count++;
+	      }
+	      return count;
+}
