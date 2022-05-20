5c4c16876a7df86630f1d621a4152a816a465d76
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-05-20 08:54:30 -0400

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,12 +23,12 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char);
-int _strlen(const char *);
-size_t print_list(const list_t *);
-size_t list_len(const list_t *);
-list_t *add_node(list_t **, const char *);
-list_t *add_node_end(list_t **, const char *);
-void free_list(list_t *);
+int _strlen(const char  *head);
+size_t print_list(const list_t  *head);
+size_t list_len(const list_t  *head);
+list_t *add_node(list_t **, const char  *head);
+list_t *add_node_end(list_t **, const char  *head);
+void free_list(list_t *head);
 
 
 #endif
