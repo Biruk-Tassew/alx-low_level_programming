dcdef24c0d4234ce8e43b4b0fc5b46d98fe6a4d6
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-05-30 14:43:48 -0400

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -7,10 +7,10 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
+ * @filename: name of the file
+ * @letters: count of the letters it should read and print
  *
- * Return: actual number of letters it could read and print
+ * Return: count of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
