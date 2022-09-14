ea8f340d2df548ac67ca82ab3d945b2ce2d134fc
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-09-14 14:51:30 -0400

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,14 +5,20 @@
 def island_perimeter(grid):
     """Calculate perimeter of a grid island. 
     """
-    m, n, Perimeter = len(grid), len(grid[0]), 0
+    if not grid:
+        return 0
 
-    for i in range(m):
-        for j in range(n):
-            Perimeter += 4*grid[i][j]
-            if i > 0:   Perimeter -= grid[i][j]*grid[i-1][j]
-            if i < m-1: Perimeter -= grid[i][j]*grid[i+1][j]
-            if j > 0:   Perimeter -= grid[i][j]*grid[i][j-1]
-            if j < n-1: Perimeter -= grid[i][j]*grid[i][j+1]
-                
-    return Perimeter
+    def sum_adjacent(i, j):
+        adjacent = (i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1),
+        res = 0
+        for x, y in adjacent:
+            if x < 0 or y < 0 or x == len(grid) or y == len(grid[0]) or grid[x][y] == 0:
+                res += 1
+        return res
+
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                count += sum_adjacent(i, j)
+    return count
